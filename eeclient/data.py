c7be5d657ea0173fb08e04b7f4fa25fe580d24af
dfguerrerom <dfgm2006@gmail.com>
2024-08-12 13:56:31 +0200

    feat: preprocess objects to display in map

--- eeclient/data.py
+++ eeclient/data.py
@@ -1,3 +1,4 @@
+from typing import List, TypedDict, Union
 from ee.ee_exception import EEException
 
 import json
@@ -6,30 +7,123 @@ from ee import serializer
 from ee import _cloud_api_utils
 
 from ee.data import TileFetcher
-from ipyleaflet import TileLayer
 
 from eeclient.client import Session
 
 
-def get_map_id(session: Session, ee_image: ee.Image):
+class MapTileOptions(TypedDict):
+    """
+    MapTileOptions defines the configuration for map tile generation.
+
+    Keys:
+        min (str or List[str]): Comma-separated numbers representing the values
+            to map onto 00. It can be a string of comma-separated numbers
+            (e.g., "1,2,3") or a list of strings. (e.g., ["1", "2", "3"]).
+        max (str or List[str]): Comma-separated numbers representing the values
+            to map onto FF. It can be a string of comma-separated numbers or
+            a list of strings.
+        gain (str or List[str]): Comma-separated numbers representing the gain
+            to map onto 00-FF. It can be a string of comma-separated numbers or
+            a list of strings.
+        bias (str or List[str]): Comma-separated numbers representing the
+            offset to map onto 00-FF. It can be a string of comma-separated
+            numbers or a list of strings.
+        gamma (str or List[str]): Comma-separated numbers representing the
+            gamma correction factor. It can be a string of comma-separated
+            numbers or a list of strings.
+        palette (str): A string of comma-separated CSS-style color strings
+            (single-band previews only).For example, 'FF0000,000000'.
+        format (str): The desired map tile format.
+    """
+
+    min: Union[str, List[str]]
+    max: Union[str, List[str]]
+    gain: Union[str, List[str]]
+    bias: Union[str, List[str]]
+    gamma: Union[str, List[str]]
+    palette: str
+    format: str
+
+
+def get_ee_image(
+    ee_object: Union[ee.Image, ee.ImageCollection, ee.Feature, ee.FeatureCollection],
+    vis_params: MapTileOptions = {},
+):
+    """Convert an Earth Engine object to a image request object"""
+
+    def get_image_request(ee_image: ee.Image, vis_params={}):
+
+        vis_image, request = ee_image._apply_visualization(vis_params)
+        request["image"] = vis_image
+
+        return request
+
+    if isinstance(ee_object, ee.Image):
+        return get_image_request(ee_object, vis_params)
+
+    elif isinstance(ee_object, ee.ImageCollection):
+
+        ee_image = ee_object.mosaic()
+        return get_image_request(ee_image, vis_params)
+
+    elif isinstance(ee_object, ee.Feature):
+        ee_image = ee.FeatureCollection(ee_object).draw(
+            color=(vis_params or {}).get("color", "000000")
+        )
+        return get_image_request(ee_image)
+
+    elif isinstance(ee_object, ee.FeatureCollection):
+        ee_image = ee_object.draw(color=(vis_params or {}).get("color", "000000"))
+        return get_image_request(ee_image)
+
+    else:
+        raise ValueError("Invalid ee_object type")
+
+
+def get_map_id(
+    session: Session,
+    ee_image: ee.Image,
+    vis_params: MapTileOptions = None,
+    bands: str = None,
+    format: str = None,
+):
+    """Get the map id of an image
+
+    Args:
+        session: The session object
+        ee_image: The image to get the map id of
+        vis_params (Optional[MapTileOptions]): The visualization parameters,
+            such as min/max values, gain, bias, gamma correction,
+        bands: The bands to display
+            palette, and format. Refer to the MapTileOptions type for details.
+        format: A string describing an image file format that was passed to one
+            of the functions in ee.data that takes image file formats
+    """
+
+    ee_image_request = get_ee_image(ee_image)
+
+    # renname
+    format_ = format
 
     url = "https://earthengine.googleapis.com/v1alpha/projects/{project}/maps"
 
     request_body = {
-        "expression": serializer.encode(ee_image, for_cloud_api=True),
-        "fileFormat": _cloud_api_utils.convert_to_image_file_format(None),
-        "bandIds": _cloud_api_utils.convert_to_band_list(None),
+        "expression": serializer.encode(ee_image_request, for_cloud_api=True),
+        "fileFormat": _cloud_api_utils.convert_to_image_file_format(format_),
+        "bandIds": _cloud_api_utils.convert_to_band_list(bands),
     }
+
+    visualization_options = _cloud_api_utils.convert_to_visualization_options(
+        vis_params
+    )
+
+    if visualization_options:
+        request_body["visualizationOptions"] = visualization_options
 
     request_body = json.dumps(request_body)
 
     response = session.rest_call("POST", url, data=request_body)
     map_name = response["name"]
-
-    return map_name
-
-
-def get_map_tile(map_name: str):
 
     _tile_base_url = "https://earthengine.googleapis.com"
     version = "v1"
@@ -39,22 +133,24 @@ def get_map_tile(map_name: str):
         version,
         map_name,
     )
-
-    map_id = {
+    return {
         "mapid": map_name,
         "token": "",
         "tile_fetcher": TileFetcher(url_format, map_name=map_name),
     }
 
-    return TileLayer(
-        url=map_id["tile_fetcher"].url_format,
-        attribution="Google Earth Engine",
-        name="name",
-        max_zoom=24,
-    )
+
+# def get_map_tile(map_name: str):
+
+#     return TileLayer(
+#         url=map_id["tile_fetcher"].url_format,
+#         attribution="Google Earth Engine",
+#         name="name",
+#         max_zoom=24,
+#     )
 
 
-def get_info(session: Session, ee_object, workloadTag=None):
+def get_info(session: Session, ee_object: ee.ComputedObject, workloadTag=None):
     """Get the info of an Earth Engine object"""
 
     data = {
@@ -64,7 +160,7 @@ def get_info(session: Session, ee_object, workloadTag=None):
 
     url = "https://earthengine.googleapis.com/v1/projects/{project}/value:compute"
 
-    return session.rest_call("POST", url, data=data)
+    return session.rest_call("POST", url, data=data)["result"]
 
 
 def get_asset(session: Session, ee_asset_id: str):
@@ -89,4 +185,3 @@ class EERestException(EEException):
 
 getInfo = get_info
 getAsset = get_asset
-getMapTile = get_map_tile
